4e784f712d9633e5843dfeb44165004338027165
LilasAll <lilas.allard@gmail.com>
2019-11-18 15:44:29 +0100

    adding exo 6

--- script.js
+++ script.js
@@ -2,12 +2,65 @@
 //Affiche clique dans la console quand on clique sur le footer
 //Puis compte le nombre de clique
 var footer = document.querySelector('footer');
-footer.addEventListener('click', seeText);
+footer.addEventListener('click', footerClick);
+clickCount = 0
 
-function seeText() {
-	console.log("clique")
+function footerClick() {
+	console.log("clic")
+	clickCount++
+	console.log("clic numéro " + clickCount)
 }
 
-footer.addEventListener('click', seeText)
-console.log()
+footer.addEventListener('click', footerClick);
 
+//FONCTION 2 - Hamburger Menu
+var menu = document.getElementById('navbarHeader')
+var menuBtn = document.getElementsByClassName('navbar-toggler')[0];
+
+function hamburger(){
+	menu.classList.toggle('collapse');
+}
+
+menuBtn.addEventListener('click', hamburger);
+
+//FONCTION 3 - Card 1 color in red
+var firstCard = document.getElementsByClassName('card')[0];
+var editBtn1 = document.getElementsByClassName('btn-outline-secondary')[0];
+
+function editBtnChange() {
+	firstCard.style.color = 'red';
+}
+
+editBtn1.addEventListener('click', editBtnChange)
+
+//FONCTION 4 - Card 2 color green 
+var secondCard = document.getElementsByClassName('card')[1];
+var editBtn2 = document.getElementsByClassName('btn-outline-secondary')[1];
+
+function editBtnChange2() {
+	if (secondCard.style.color === 'green') {
+		secondCard.style.color = '';
+	}
+	else  {
+		secondCard.style.color = 'green';
+	}
+}
+
+editBtn2.addEventListener('click', editBtnChange2);
+
+//FONCTION 5 - Bootstrap delete
+var bootstrapLink = document.querySelector('link');
+var navbar = document.querySelector('header')
+
+function bootstrapDelete () {
+	if (bootstrapLink.disabled === false) {
+		bootstrapLink.disabled = true;
+	}
+	else {
+		bootstrapLink.disabled = false;
+	}
+}
+
+navbar.addEventListener('dblclick', bootstrapDelete)
+
+//FONCTION 6 - 
